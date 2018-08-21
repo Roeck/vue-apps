f814666c4dab9e3d583c68bb19b4612667ee2a66
Roeck <rons9n@gmail.com>
2018-08-21 16:48:03 -0400

    switchitem method

--- applications/starbase-router/src/components/Data.vue
+++ applications/starbase-router/src/components/Data.vue
@@ -1,10 +1,16 @@
 <template>
-  <div class="">
-   {{items}}
- </div>
+<div class="col-md-12">
+  <Item
+    v-for="(item, index) in items"
+    key="index"
+    :passed-item="item"
+    :type="type"
+  />
+</div>
 </template>
 
 <script>
+import Item from './Item.vue'
 export default {
   data() {
     return {
@@ -18,21 +24,24 @@ export default {
   methods: {
     fetchItems() {
       this.items = []
+      this.type = this.$route.params.type
       let initial_ids = [1, 13, 14]
-
-      for  (let i in initial_ids) {
+      for (let i in initial_ids) {
         let id = initial_ids[i]
         console.log('id', id)
-        fetch(`http://swapi.co/api/${this.type}/${id}`,
+        fetch(`http://swapi.co/api/${this.type}/${id}`, {
           method: 'GET'
-      })
+        })
           .then(response => response.json())
           .then(json => this.items.push(json))
+      }
     }
-  }
-},
+  },
   created() {
     this.fetchItems()
+  },
+  components: {
+    Item
   }
 }
 </script>

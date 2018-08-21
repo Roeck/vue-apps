149c1d6e6213525b761125fad5e27b1bffc390a6
Roeck <rons9n@gmail.com>
2018-08-21 15:41:35 -0400

    linking pages

--- applications/starbase-router/src/App.vue
+++ applications/starbase-router/src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div id="app">
     <h3>Starbase Router</h3>
+    <h4>
+      <router-link class="link" to="/data/people">The Characters</router-link> |
+      <router-link class="link" to="/data/planets">The Planets</router-link>
+    </h4>
     <router-view></router-view>
   </div>
 </template>

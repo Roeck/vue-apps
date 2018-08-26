b78057708fed899595467077d4f1acaae70ba728
Roeck <rons9n@gmail.com>
2018-08-26 16:40:50 -0400

    add firebase

---
+++ applications/turnout/src/firebaseApp.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase'
+
+const config = {
+    apiKey: "AIzaSyDCNNgPgX7H6BglSvBnppTMfluMYqyCCRc",
+    authDomain: "turnout-9.firebaseapp.com",
+    databaseURL: "https://turnout-9.firebaseio.com",
+    projectId: "turnout-9",
+    storageBucket: "turnout-9.appspot.com",
+    messagingSenderId: "814720129084"
+  };
+
+export const firebaseApp = firebase.initializeApp(config)
